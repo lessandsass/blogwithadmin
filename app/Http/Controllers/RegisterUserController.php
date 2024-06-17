d79ad63ab41c6db4b9b7a8abcf85ef3bbf69a619
Less <31947204+lessandsass@users.noreply.github.com>
2024-06-17 06:22:27 +0530

    Register functionlality
    A register route was created. RegisterUserController was created. Register and store functions were made inside the RegisterUserController.

---
+++ app/Http/Controllers/RegisterUserController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class RegisterUserController extends Controller
+{
+    public function register()
+    {
+        return view('auth.register');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255|min:3',
+            'email' => 'required|string|email|max:255|min:3|unique:users,email',
+            'password' => 'required|string|min:6|max:12|confirmed',
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+
+        auth()->login($user);
+        return to_route('posts.index');
+
+    }
+
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\LoginUserController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\RegisterUserController;
 
 Route::view('/', 'welcome')->name('home');
 
@@ -12,3 +14,11 @@ Route::post('/posts/store', [PostController::class, 'store'])->name('posts.store
 Route::get('/posts/{post}/edit', [PostController::class, 'edit'])->name('posts.edit');
 Route::put('/posts/{post}/update', [PostController::class, 'update'])->name('posts.update');
 Route::delete('/posts/{post}', [PostController::class, 'destroy'])->name('posts.destroy');
+
+Route::get('/register', [RegisterUserController::class, 'register'])->name('register');
+Route::post('/register', [RegisterUserController::class, 'store'])->name('register.store');
+
+Route::get('/register', [RegisterUserController::class, 'register'])->name('register');
+Route::post('/register', [RegisterUserController::class, 'store'])->name('register.store');
+
+Route::post('/logout', [LoginUserController::class, 'logout'])->name('logout');
