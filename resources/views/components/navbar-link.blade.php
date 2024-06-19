e85a7671b1311552e9e41eb40887c708594165fb
Less <31947204+lessandsass@users.noreply.github.com>
2024-06-20 01:38:28 +0530

    Update navbar-link.blade.php
    navbar-link.blade.php was updated.

--- resources/views/components/navbar-link.blade.php
+++ resources/views/components/navbar-link.blade.php
@@ -4,13 +4,14 @@
     <a
         {{ $attributes }}
         @class([
-            'block py-2 px-3 rounded md:p-0',
-            'text-white bg-blue-700 md:bg-transparent md:text-blue-700 dark:text-white md:dark:text-blue-500' => $active,
-            'text-gray-900 hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent' => !$active,
+            'block py-2 px-3 rounded',
+            'text-blue-500' => $active,
+            'text-white hover:text-blue-500' => !$active,
         ])
-        class="block py-2 px-3  rounded md:bg-transparent md:text-blue-700 md:p-0 "
+        class="block py-2 px-3 rounded"
         aria-current="page"
     >
         {{ $slot }}
     </a>
 </li>
+
